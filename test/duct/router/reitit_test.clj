daed09d21031a9976b6b77d404c85b7b17c3c4f9
James Reeves <jreeves@weavejester.com>
2024-10-26 12:01:05 +0100

    Initial commit

---
+++ test/duct/router/reitit_test.clj
@@ -0,0 +1,7 @@
+(ns duct.router.reitit-test
+  (:require [clojure.test :refer :all]
+            [duct.router.reitit :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
