7707fb91685c2bfa6ba154c4dc6da8c6191ec8ff
James Reeves <jreeves@weavejester.com>
2024-10-31 14:16:05 +0000

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.duct-framework/router.reitit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods for the Reitit routing library"
+  :url "https://github.com/duct-framework/router.reitit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
