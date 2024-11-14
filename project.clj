775a79d52aab5f93a62d7c1333bedf14a3da346d
James Reeves <jreeves@weavejester.com>
2024-11-14 02:15:10 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
-                 [integrant "0.13.0"]
+                 [integrant "0.13.1"]
                  [metosin/reitit "0.7.2"]]
   :repl-options {:init-ns duct.router.reitit})
